2459446954da2147b55664bd3ccb4952322fb53c
Yash Srivastava <akarsh1888@gmail.com>
2020-09-12 04:15:11 +0530

    Master Branch Cleanup

--- codelabs/myapp/database/index.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var mongodb = "mongodb://localhost:27017/my_db";
-
-mongoose.connect(mongodb, { useNewUrlParser: true });
-
-var db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB Connection error"));
--- codelabs/routing/server.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-var handleRequest = require("./handleRequest");
-
-http
-  .createServer(handleRequest.handleRequest)
-  .listen(8000, () => console.log("iam listening 8000"));
